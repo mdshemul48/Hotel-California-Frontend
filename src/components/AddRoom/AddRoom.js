b722de67f309f0b09a6ea80530a7d162c9226655
MD. Shimul <mdshemul480@gmail.com>
2021-10-31 12:46:54 +0600

    adding add room

--- src/components/AddRoom/AddRoom.js
+++ src/components/AddRoom/AddRoom.js
@@ -1,28 +1,85 @@
 import React from 'react';
+import axios from 'axios';
 import { Button, Container, Form } from 'react-bootstrap';
 
 const AddRoom = () => {
+  const [room, setRoom] = React.useState({
+    name: '',
+    description: '',
+    price: '',
+    image: '',
+  });
+
+  const RoomStateChangeHandler = (event) => {
+    setRoom({
+      ...room,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  console.log(process.env.REACT_APP_BACKEND_LINK);
+  const RoomSubmitHandler = async (event) => {
+    event.preventDefault();
+
+    try {
+      setRoom({
+        name: '',
+        description: '',
+        price: '',
+        image: '',
+      });
+      const response = await axios.post(
+        `${process.env.REACT_APP_BACKEND_LINK}/api/rooms`,
+        room
+      );
+      alert(response.data.result);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+  console.log(room);
   return (
     <main className='my-5'>
       <Container>
         <h2>Add New Room</h2>
         <hr />
-        <Form>
+        <Form onSubmit={RoomSubmitHandler}>
           <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
             <Form.Label>Room Name </Form.Label>
-            <Form.Control type='text' />
+            <Form.Control
+              name='name'
+              onChange={RoomStateChangeHandler}
+              value={room.name}
+              type='text'
+            />
           </Form.Group>
           <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
             <Form.Label>Price</Form.Label>
-            <Form.Control type='text' />
+            <Form.Control
+              name='price'
+              onChange={RoomStateChangeHandler}
+              type='text'
+              value={room.price}
+            />
           </Form.Group>
           <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
             <Form.Label>Image Link </Form.Label>
-            <Form.Control type='text' />
+            <Form.Control
+              name='image'
+              onChange={RoomStateChangeHandler}
+              type='text'
+              value={room.image}
+            />
           </Form.Group>
           <Form.Group className='mb-3' controlId='exampleForm.ControlTextarea1'>
             <Form.Label>Description</Form.Label>
-            <Form.Control as='textarea' rows={3} />
+            <Form.Control
+              onChange={RoomStateChangeHandler}
+              name='description'
+              as='textarea'
+              rows={3}
+              value={room.description}
+            />
           </Form.Group>
           <Button variant='danger' type='submit'>
             Submit
